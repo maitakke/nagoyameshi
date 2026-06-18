1d9410c6947af6b1ae02fb33e02b7a826b7a5d8a
maitakke <maitakke.maittake@gmail.com>
2026-06-18 14:12:22 +0900

    feature-home

--- src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
+++ src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
@@ -10,5 +10,5 @@ import com.example.nagoyameshi.entity.Category;
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
 	public Page<Category> findByNameLike(String keyword, Pageable pageable);
 	public Category findFirstByOrderByIdDesc();
-
+	public Category findFirstByName(String name);
 }
--- src/main/java/com/example/nagoyameshi/service/CategoryService.java
+++ src/main/java/com/example/nagoyameshi/service/CategoryService.java
@@ -49,7 +49,12 @@ public class CategoryService {
    // すべてのカテゴリをリスト形式で取得する
    public List<Category> findAllCategories() {
        return categoryRepository.findAll();
-   }   
+   }
+   
+   // 指定したカテゴリ名を持つ最初のカテゴリを取得する
+   public Category findFirstCategoryByName(String name) {
+	   return categoryRepository.findFirstByName(name);
+   }
    
    @Transactional
    public void createCategory(CategoryRegisterForm categoryRegisterForm) {
